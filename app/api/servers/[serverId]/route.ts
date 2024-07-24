ad0d6a50790d39fc0684a9ebe5dbc479823ac22a
bachtv1509 <bachtv150902@gmail.com>
2024-07-24 21:00:17 +0700

    setup

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const { name, imageUrl } = await req.json();
+    const profile = await currentProfile();
+    if (!profile) return new NextResponse("Unauthozied", { status: 401 });
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVERS_ID_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) return new NextResponse("Unauthozied", { status: 401 });
+
+    if (!params.serverId)
+      return new NextResponse("Server ID missing", { status: 400 });
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVERS_ID_LEAVE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
